15a8d8c5b393af88c6593157851d9e265df5dd5d
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-11-24 01:25:54 +0530

    RMA

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 const dbConnect = require("./config/db");
 const userRouter = require("./routes/userRoutes");
 const recipeRouter = require("./routes/recipeRoutes");
+
 const app = express();
 require("dotenv").config();
 
@@ -13,6 +15,7 @@ app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.use("/user", userRouter);
 app.use("/recipe", recipeRouter);
