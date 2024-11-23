978778e66390fb7cf76ca836d1634a3dd0d25cad
Divyesh1692 <divyeshvariya1692@gmail.com>
2024-11-24 01:18:24 +0530

    RMA

--- server.js
+++ server.js
@@ -1,30 +1,45 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const dbConnect = require("./config/db");
+const userRouter = require("./routes/userRoutes");
+const recipeRouter = require("./routes/recipeRoutes");
 const app = express();
-
 require("dotenv").config();
 
+// Middleware
 app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-const userRouter = require("./routes/userRouter");
-const recipeRouter = require("./routes/recipeRouter");
-
+// API Routes
 app.use("/user", userRouter);
 app.use("/recipe", recipeRouter);
 
+// MongoDB Connection
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log("Error connecting to MongoDB:", err));
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+    process.exit(1); // Exit process with failure
+  });
 
+// Basic Route (Health check)
 app.get("/", (req, res) => {
-  res.send("Recipe Management App is running!");
+  res.send("Recipe Management App is running");
 });
 
+// Error Handling Middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something went wrong!");
+});
+
+// Export for serverless functions
 module.exports = app;
